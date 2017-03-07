d9f2cfaa729bfd6212b5f23816b867aac54ae03c
Grigory Pomadchin <gr.pomadchin@gmail.com>
2017-03-07 17:49:04 +0300

    +cors support; + new route

--- server/src/main/scala/com/azavea/server/Router.scala
+++ server/src/main/scala/com/azavea/server/Router.scala
@@ -1,19 +1,20 @@
 package com.azavea.server
 
-import geotrellis.proj4.{LatLng, WebMercator}
+import geotrellis.proj4.LatLng
 import geotrellis.raster._
 import geotrellis.raster.render._
+import geotrellis.raster.io.geotiff.GeoTiff
+import geotrellis.raster.io.geotiff.writer.GeoTiffWriter
 import geotrellis.spark._
 import geotrellis.spark.io._
+import geotrellis.spark.stitch._
 import geotrellis.vector.io.json.Implicits._
 import geotrellis.vector.Polygon
-import geotrellis.vector.reproject._
 
 import org.apache.spark.{SparkConf, SparkContext}
+import ch.megard.akka.http.cors.CorsDirectives._
 import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.model.{ContentType, HttpEntity, HttpResponse, MediaTypes}
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import spray.json._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -52,26 +53,73 @@ trait Router extends Directives with AkkaSystem.LoggerExecutor {
       pathPrefix("png") {
         pathPrefix(Segment / IntNumber / IntNumber / IntNumber) { (layerName, zoom, x, y) =>
           parameters('poly ? "") { poly =>
+            cors() {
+              val layerId = LayerId(layerName, zoom)
+              val key = SpatialKey(x, y)
+              val md = attributeStore.readMetadata[TileLayerMetadata[SpatialKey]](layerId)
+              val extent = md.mapTransform(key)
+              val polygon =
+                if (poly.isEmpty) None
+                else Some(poly.parseGeoJson[Polygon].reproject(LatLng, md.crs))
+
+              complete {
+                Future {
+                  val tileOpt =
+                    try {
+                      Some(tileReader.reader[SpatialKey, MultibandTile](layerId).read(key))
+                    } catch {
+                      case e: ValueNotFoundError =>
+                        None
+                    }
+                  tileOpt.map { tile =>
+                    val bytes = polygon.fold(tile) { p => tile.mask(extent, p.geom) }.renderPng().bytes
+                    HttpResponse(entity = HttpEntity(ContentType(MediaTypes.`image/png`), bytes))
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    } ~
+    pathPrefix("layer") {
+      pathPrefix(Segment / IntNumber) { (layerName, zoom) =>
+        parameters('poly ?, 'bands ?, 'format ? "tiff") { (poly, bands, format) =>
+          cors() {
             val layerId = LayerId(layerName, zoom)
-            val key = SpatialKey(x, y)
             val md = attributeStore.readMetadata[TileLayerMetadata[SpatialKey]](layerId)
-            val extent = md.mapTransform(key)
-            val polygon =
-              if (poly.isEmpty) None
-              else Some(poly.parseGeoJson[Polygon].reproject(LatLng, md.crs))
+            val polygon = poly.map(_.parseGeoJson[Polygon].reproject(LatLng, md.crs))
+            val bandsSeq = bands.toList.flatMap(_.split("\\.").toList.map(_.toInt))
 
             complete {
               Future {
+                val q = reader.query[SpatialKey, MultibandTile, TileLayerMetadata[SpatialKey]](layerId)
+                val layer =
+                  polygon
+                    .fold(q.result)(p => q.where(Intersects(p)).result.mask(p))
+
+                val selectedLayer =
+                  if (bandsSeq.nonEmpty) layer.withContext(_.mapValues(_.subsetBands(bandsSeq)))
+                  else layer
+
                 val tileOpt =
                   try {
-                    Some(tileReader.reader[SpatialKey, MultibandTile](layerId).read(key))
+                    Some(selectedLayer.stitch)
                   } catch {
-                    case e: ValueNotFoundError =>
+                    case e: Throwable => {
+                      e.printStackTrace()
                       None
+                    }
                   }
+
                 tileOpt.map { tile =>
-                  val bytes = polygon.fold(tile) { p => tile.mask(extent, p.geom) }.renderPng().bytes
-                  HttpResponse(entity = HttpEntity(ContentType(MediaTypes.`image/png`), bytes))
+                  val raster = polygon.fold(tile) { p => tile.crop(p.envelope) }
+                  val (ct, bytes) = format match {
+                    case "png" => MediaTypes.`image/png` -> raster.tile.renderPng().bytes
+                    case _ => MediaTypes.`image/tiff` -> GeoTiffWriter.write(GeoTiff(raster, md.crs))
+                  }
+
+                  HttpResponse(entity = HttpEntity(ContentType(ct), bytes))
                 }
               }
             }
